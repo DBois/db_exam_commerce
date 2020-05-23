7c8ed73a5d82ecdcb2cafb0da35132cfb4d76d7b
Gonron <lundsgaard1997@hotmail.dk>
2020-05-23 12:20:24 +0200

    Added Error/Exception handling to the functions/procedures in logistics db

--- postgres/create_functions_procedures_triggers_logistics.sql
+++ postgres/create_functions_procedures_triggers_logistics.sql
@@ -1,23 +1,22 @@
-DROP FUNCTION IF EXISTS get_qty_of_product, restock_qty CASCADE;
+DROP FUNCTION IF EXISTS get_qty_of_product, check_qty CASCADE;
 DROP PROCEDURE IF EXISTS add_employee, remove_employee, add_existing_item_to_stock, add_new_item_to_stock;
-DROP TRIGGER IF EXISTS qty_couter ON department_item;
+DROP TRIGGER IF EXISTS qty_couter ON department_item CASCADE;
 
 -- Functions
 
-CREATE OR REPLACE FUNCTION restock_qty() RETURNS TRIGGER 
+CREATE OR REPLACE FUNCTION check_qty() RETURNS TRIGGER 
 	LANGUAGE plpgsql AS $$
 	
 	DECLARE 
 		int_department_fk integer;
 		var_item_fk varchar;
+		int_qty int;
 	
 	BEGIN 
-		SELECT department_fk, item_fk INTO int_department_fk, var_item_fk FROM department_item di WHERE di.qty <= 2;
-		UPDATE department_item SET qty = 100 WHERE item_fk = var_item_fk AND department_fk = int_department_fk;
-		
-		IF int_department_fk IS NOT NULL THEN 
-			-- logging
-			INSERT INTO restock_logfile(department_id, item_product_no, description) VALUES (int_department_fk, var_item_fk, 'Restocked');
+		SELECT department_fk, item_fk, qty INTO int_department_fk, var_item_fk, int_qty FROM department_item di WHERE di.qty <= 2;
+	
+		IF var_item_fk IS NOT NULL THEN
+			RAISE NOTICE 'Product number: %, in Department: %, is running low! ', var_item_fk, int_department_fk;
 		END IF;
 	
 		RETURN NEW;
@@ -58,7 +57,7 @@ BEGIN
 		RETURN QUERY SELECT item.name, di.* FROM item 
 			JOIN department_item di ON di.item_fk = item.product_number
 			WHERE item.name ILIKE '%' || item_name || '%' AND di.department_fk = department_id;
-	
+		
 	ELSE
 	
 		-- Searching on product number
@@ -73,18 +72,55 @@ $$;
 
 -- Procedures
 
+CREATE OR REPLACE PROCEDURE restock_item(p_product_number varchar, p_department_number int, p_qty int) LANGUAGE plpgsql AS $$
+	
+	DECLARE 
+
+		current_qty int;
+
+	BEGIN
+		IF p_qty < 0 THEN
+			RAISE NOTICE 'You can not add a negative quantity amount to the stock';
+		ELSE 
+			SELECT qty INTO current_qty FROM department_item di 
+			WHERE item_fk = p_product_number AND department_fk = p_department_number;
+		
+			UPDATE department_item SET qty = (current_qty + p_qty) 
+			WHERE item_fk = p_product_number AND department_fk = p_department_number;
+		
+			IF (SELECT EXISTS(SELECT 1 FROM department_item WHERE item_fk = p_product_number AND department_fk = p_department_number)) THEN 
+				-- logging
+				INSERT INTO restock_logfile(department_id, item_product_no, description) 
+				VALUES (p_department_number, p_product_number, 
+				format('Product number: %s, in Department: %s, has been restocked with %s items', p_product_number, p_department_number,  p_qty));
+			END IF;
+		END IF;
+		
+	
+		-- Update on incorret fk's dose not throw an exception to catch
+		
+	END
+$$;
+
 CREATE OR REPLACE PROCEDURE add_employee(p_name varchar, p_address varchar, p_salary int, p_job_position int, p_department int) 
 	LANGUAGE plpgsql AS $$
 
 	BEGIN 
 		INSERT INTO employee (name, address, salary, job_position_fk, department_fk) 
 		VALUES (p_name, p_address, p_salary, p_job_position, p_department);
+	
+		EXCEPTION
+			WHEN SQLSTATE '23503' THEN 
+				RAISE NOTICE 'Job position id: % or department number: %, does not exist', p_job_position, p_department;
+			WHEN OTHERS THEN
+				RAISE NOTICE '% %', SQLERRM, SQLSTATE;
 	END
 $$;
 
 CREATE OR REPLACE PROCEDURE remove_employee(p_employee_id int) LANGUAGE plpgsql AS $$
 	BEGIN 
 		DELETE FROM employee WHERE id = p_employee_id;
+		-- Delete on incorrect id dose not throw an exception to catch
 	END
 $$;
 
@@ -94,7 +130,14 @@ CREATE OR REPLACE PROCEDURE add_existing_item_to_stock(p_item_number varchar, p_
 	
 	BEGIN 
 		INSERT INTO department_item(item_fk, department_fk, qty) VALUES (p_item_number, p_department_number, p_qty);
-	END
+	
+		EXCEPTION
+			WHEN SQLSTATE '23503' THEN 
+				RAISE NOTICE 'Product number: % or department number: %, does not exist', p_item_number, p_department_number;
+			WHEN OTHERS THEN
+				RAISE NOTICE '% %', SQLERRM, SQLSTATE;
+				
+	END	
 $$;
 
 
@@ -105,10 +148,19 @@ CREATE OR REPLACE PROCEDURE add_new_item_to_stock(p_product_number varchar, p_na
 	BEGIN
 		INSERT INTO item(product_number, name, description, price) VALUES (p_product_number, p_name, p_description, p_price);
 		INSERT INTO department_item(item_fk, department_fk, qty) VALUES (p_product_number, p_department_number, p_qty);
+ 	
+ 		EXCEPTION
+ 			WHEN SQLSTATE '23505' THEN
+ 				RAISE NOTICE 'Product number: % already exist', p_product_number;
+			WHEN SQLSTATE '23503' THEN 
+				RAISE NOTICE 'Department number: %, does not exist', p_department_number;
+			WHEN OTHERS THEN
+				RAISE NOTICE '% %', SQLERRM, SQLSTATE;
+ 	
  	END
  $$;
 
 -- Triggers
 
 CREATE TRIGGER qty_counter AFTER UPDATE ON department_item 
-FOR EACH ROW EXECUTE FUNCTION restock_qty();
+FOR EACH ROW EXECUTE FUNCTION check_qty();
