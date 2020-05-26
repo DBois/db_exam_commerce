d174bbe42af453463fb2c54dff82b87b18a8305a
Gonron <lundsgaard1997@hotmail.dk>
2020-05-26 12:19:19 +0200

    Updated insert_creditcard procedure

--- postgres/create_functions_procedures_triggers_customer.sql
+++ postgres/create_functions_procedures_triggers_customer.sql
@@ -1,9 +1,10 @@
 DROP PROCEDURE IF EXISTS insert_creditcard;
 
-CREATE OR REPLACE PROCEDURE insert_creditcard(p_card_number varchar, p_expiration_date varchar, p_customer_fk int) LANGUAGE plpgsql AS $$
+CREATE OR REPLACE PROCEDURE insert_creditcard(p_card_number bpchar, p_expiration_date bpchar, p_name varchar) LANGUAGE plpgsql AS $$
 	BEGIN 
-		IF p_card_number ~ '^[0-9\.]{16}' AND p_expiration_date ~ '^[0-9\.]{4}'  THEN 
-			INSERT INTO credit_card(card_number, expiration_date, customer_fk) VALUES (p_card_number, p_expiration_date, p_customer_fk);
+		IF p_card_number ~ '^[0-9\.]{16}' AND p_expiration_date ~ '^[0-9\.]{4}' THEN 
+			INSERT INTO credit_card(card_number, expiration_date, name) VALUES (p_card_number, p_expiration_date, p_name);
 		END IF;
+		RAISE NOTICE 'Cardnumber: % or expiration date: % is to short', p_card_number, p_expiration_date;
 	END
 $$;
