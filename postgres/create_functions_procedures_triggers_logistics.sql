a9113df7c3c8ac14d9c33945b15d23f1a28f8845
Adam Saidane <adamsaidane@hotmail.com>
2020-05-29 14:55:59 +0200

    Moved get_employees function into the big boi script

--- postgres/create_functions_procedures_triggers_logistics.sql
+++ postgres/create_functions_procedures_triggers_logistics.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION IF EXISTS get_qty_of_product, get_qty_of_product_in_department, check_qty CASCADE;
+DROP FUNCTION IF EXISTS get_qty_of_product, get_qty_of_product_in_department, check_qty, get_employees CASCADE;
 DROP PROCEDURE IF EXISTS add_employee, remove_employee, add_existing_product_to_stock, add_new_product_to_stock, restock_product;
 DROP TRIGGER IF EXISTS qty_couter ON department_product CASCADE;
 
@@ -69,6 +69,23 @@ BEGIN
 		
 END
 $$;
+
+create or replace function get_employees(department_id int)
+returns table (
+    id int,
+	name VARCHAR,
+	salary int,
+	address VARCHAR,
+	title VARCHAR,
+	dept_id int
+)
+as $$
+BEGIN
+return query select e.id, e.name, e.salary, e.address, jp.title, e.department_fk 
+from employee e join job_position jp 
+on e.job_position_fk  = jp.id where e.department_fk = department_id;
+end; $$
+language 'plpgsql';
 
 -- Procedures
 
